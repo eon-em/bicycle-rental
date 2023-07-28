720ec1cd1b81ee37ff45665a9a69eb9f82488e1a
eon-em <eon.epqm@gmail.com>
2023-07-28 14:11:47 -0300

    Adding Spring project boilerplate

---
+++ src/main/java/br/ufscar/dc/dsw/JEGBicycles/JegBicyclesApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.JEGBicycles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JegBicyclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JegBicyclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufscar/dc/dsw/JEGBicycles/JegBicyclesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.JEGBicycles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JegBicyclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
